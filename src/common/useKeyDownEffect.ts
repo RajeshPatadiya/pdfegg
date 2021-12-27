805f2c4858b363379cb52166970af90ec51e25af
Rustem Kakimov <ruskakimov@gmail.com>
2021-12-27 23:57:32 +0600

    Only prevent default for handled key events

--- src/common/useKeyDownEffect.ts
+++ src/common/useKeyDownEffect.ts
@@ -21,8 +21,6 @@ function useKeyDownEffect(
   callback: () => void
 ): void {
   const keyListener = (e: KeyboardEvent) => {
-    e.preventDefault();
-
     const currentCombination: KeyCombination = {
       ctrl: e.ctrlKey || e.metaKey,
       alt: e.altKey,
@@ -31,6 +29,7 @@ function useKeyDownEffect(
     };
 
     if (sameCombination(currentCombination, triggerCombination)) {
+      e.preventDefault();
       callback();
     }
   };
