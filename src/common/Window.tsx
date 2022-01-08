22c5567ad1824f101c5ae155955f743630da5518
Rustem Kakimov <ruskakimov@gmail.com>
2022-01-08 17:38:18 +0600

    Extract get visible range

--- src/common/Window.tsx
+++ src/common/Window.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface WindowProps {
   itemCount: number;
@@ -24,6 +24,10 @@ function Window(props: WindowProps) {
 
   // TODO: Update visible on first load
 
+  useEffect(() => {
+    console.log(itemsRef.current);
+  }, []);
+
   const children = Array(props.itemCount)
     .fill(null)
     .map((_, index) =>
@@ -36,60 +40,73 @@ function Window(props: WindowProps) {
     <div
       style={{ height: "100%", overflow: "scroll" }}
       onScroll={(e) => {
-        const viewport = e.currentTarget;
-        const viewportTop = viewport.scrollTop;
-        const viewportBottom = viewportTop + viewport.clientHeight;
-        const items = itemsRef.current;
-
         // Validate pre-conditions:
-        const preConditions = [items.length === props.itemCount];
-
+        const preConditions = [itemsRef.current.length === props.itemCount];
         if (preConditions.includes(false)) {
           throw Error("Window: Invalid pre-conditions " + preConditions);
         }
 
-        function isItemVisible(item: HTMLElement): boolean {
-          const itemTop = item.offsetTop - viewport.offsetTop;
-          const itemBottom = itemTop + item.clientHeight;
-          const invisible =
-            itemBottom <= viewportTop || itemTop >= viewportBottom;
-          return !invisible;
-        }
-
-        // TODO: Optimize the algorithm, most of the time visible range will remain unchanged
-        const newStart = items.findIndex((item) => isItemVisible(item!));
-        const newEnd =
-          items.length -
-          1 -
-          items
-            .slice()
-            .reverse()
-            .findIndex((item) => isItemVisible(item!));
-
-        // Validate post-conditions:
-        const postConditions = [
-          isItemVisible(items[newStart]!),
-          isItemVisible(items[newEnd]!),
-          newStart === 0 || !isItemVisible(items[newStart - 1]!),
-          newEnd === items.length - 1 || !isItemVisible(items[newEnd + 1]!),
-        ];
-
-        if (postConditions.includes(false)) {
-          throw Error("Window: Invalid post-conditions " + postConditions);
-        }
+        const newVisibleRange = getVisibleItemRange(
+          e.currentTarget,
+          itemsRef.current
+        );
 
         if (
-          newStart !== visibleRange.startIndex ||
-          newEnd !== visibleRange.endIndex
+          newVisibleRange.startIndex !== visibleRange.startIndex ||
+          newVisibleRange.endIndex !== visibleRange.endIndex
         ) {
-          props.onVisibleChanged(newStart, newEnd);
-          setVisibleRange({ startIndex: newStart, endIndex: newEnd });
+          props.onVisibleChanged(
+            newVisibleRange.startIndex,
+            newVisibleRange.endIndex
+          );
+          setVisibleRange(newVisibleRange);
         }
       }}
     >
       {children}
     </div>
   );
+}
+
+function getVisibleItemRange(
+  viewport: HTMLElement,
+  items: HTMLElement[]
+): VisibleRange {
+  const viewportTop = viewport.scrollTop;
+  const viewportBottom = viewportTop + viewport.clientHeight;
+
+  function isItemVisible(item: HTMLElement): boolean {
+    const itemTop = item.offsetTop - viewport.offsetTop;
+    const itemBottom = itemTop + item.clientHeight;
+    const invisible = itemBottom <= viewportTop || itemTop >= viewportBottom;
+    return !invisible;
+  }
+
+  // TODO: Optimize the algorithm, most of the time visible range will remain unchanged
+  const newStart = items.findIndex((item) => isItemVisible(item!));
+  const newEnd =
+    items.length -
+    1 -
+    items
+      .slice()
+      .reverse()
+      .findIndex((item) => isItemVisible(item!));
+
+  // Validate post-conditions:
+  const postConditions = [
+    isItemVisible(items[newStart]!),
+    isItemVisible(items[newEnd]!),
+    newStart === 0 || !isItemVisible(items[newStart - 1]!),
+    newEnd === items.length - 1 || !isItemVisible(items[newEnd + 1]!),
+  ];
+  if (postConditions.includes(false)) {
+    throw Error("Window: Invalid post-conditions " + postConditions);
+  }
+
+  return {
+    startIndex: newStart,
+    endIndex: newEnd,
+  };
 }
 
 export default Window;
